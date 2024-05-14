8eb9d9ccd37c9046c8c025160c84628a7df9fe34
PeshO2K <patienceotuke@gmail.com>
2024-05-14 16:07:55 +0300

    Pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,30 +9,16 @@ def pascal_triangle(n):
         rows.append([1])
         for row_num in range(1, n):
             new_row = []
-            prev_row = rows[row_num-1]
+            prev_row = rows[row_num - 1]
             m = len(prev_row)
-            for i in range(m+1):
-                if i==0:
+            for i in range(m + 1):
+                if i == 0:
                     new_row.append(prev_row[i])
-                elif i==m:
+                elif i == m:
                     new_row.append(prev_row[i-1])
                 else:
                     new_row.append(prev_row[i-1]+prev_row[i])
 
             rows.append(new_row)
 
-            """ row = rows[row_num - 1]
-            new_row = []
-            m = len(row)
-            for i in range(m + 1):
-                x = i - 1
-                if x < 0 or i == m:
-                    if i == m:
-                        idx = i - 1
-                    else:
-                        idx = i
-                    new_row.append(row[idx] + 0)
-                else:
-                    new_row.append(row[x] + row[i])
-            rows.append(new_row) """
-    return rows 
+    return rows
